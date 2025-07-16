d7ee297f306f5a99e0bce0d3251dd83a4be2f4a6
shivamkumar854 <shivamkumar3210hjp@gmail.com>
2025-07-16 18:53:20 +0530

    fixed backend url

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstore-backend-nc5i.onrender.com/book");
         const data = res.data.filter((data) => data.category === "Free");
         setBook(data);
       } catch (error) {
